c96e1d33759ab5202ea02397006bd24ac871e6c3
Olivier Lalonde <olalonde@gmail.com>
2016-06-15 13:53:19 -0700

    isMerkleRootValid, isTargetHashValid

--- src/index.js
+++ src/index.js
@@ -1,5 +1,72 @@
-import merkletree from 'merkletree'
+/* eslint-disable no-unused-vars */
+import merkletree, { verifyProof } from 'merkletree'
+import { createHash } from 'crypto'
 
-export default verify = () => {
-  return false
+const blockaiVerify = (_proof) => {
+  const proof = _proof.proof ? _proof.proof : _proof
+  const {
+    header: {
+      hash_type,
+      merkle_root,
+      tx_id,
+      timestamp,
+    },
+    target: {
+      target_hash,
+      target_proof,
+    },
+    extras,
+  } = proof
+
+  const hashAlgorithm = (hash_type || 'sha256').replace('-', '')
+
+  const hash = (input) => {
+    const hasher = createHash(hashAlgorithm)
+    return hasher.update(input).digest('hex')
+  }
+
+  // Sync method to check if target hash is valid with respect to
+  // extras.leaves
+  const isTargetHashValid = () => {
+    const leaves = extras.leaves.map((maybeLeaf) => {
+      const leaf = typeof maybeLeaf === 'string'
+        ? maybeLeaf
+        : hash(maybeLeaf.data)
+      return leaf
+    })
+    const tree = merkletree(leaves)
+    const root = tree.root()
+    return root === target_hash
+  }
+
+  // Sync method to check if merkle root is valid with respect
+  // to target_proof
+  const isMerkleRootValid = () => verifyProof(target_hash, merkle_root, target_proof)
+
+  // Async method to verify if downloading dataUrl produces
+  // expected hash
+  const isDataHashValid = () => {
+
+  }
+
+  // Async method that checks how many confirmations transaction
+  // holding merkle root has
+  const getConfirmations = () => {
+
+  }
+
+  // Combines method above to give detailed report about proof
+  const analyze = () => {
+
+  }
+
+  return {
+    isTargetHashValid,
+    isMerkleRootValid,
+    isDataHashValid,
+    getConfirmations,
+    analyze,
+  }
 }
+
+export default blockaiVerify
--- test/index.js
+++ test/index.js
@@ -1,7 +1,62 @@
 import test from 'blue-tape'
-import merkletree from 'merkletree'
+import blockaiVerify from '../src'
+import fs from 'fs'
+import path from 'path'
+import express from 'express'
 
-test((t) => {
+const testDataPath = path.join(__dirname, 'data/')
+
+// Load test data
+const loadTestData = (name, baseUrl) => {
+  const basePath = path.join(testDataPath, name)
+  // const dataPath = path.join(basePath, 'file')
+  const proofPath = path.join(basePath, 'proof.json')
+
+  const proof = JSON.parse(fs.readFileSync(proofPath, { encoding: 'utf8' }))
+  // const dataBuffer = fs.readFileSync(dataPath)
+  proof.extras.dataUrl = `${baseUrl}/${name}/file`
+  return {
+    proof,
+  }
+}
+
+const loadAllTestData = (baseUrl) => [
+  'confirmed',
+  'invalid',
+].reduce((acc, name) => ({
+  ...acc,
+  [name]: loadTestData(name, baseUrl),
+}), {})
+
+let testData
+let httpServer
+test('start http server to serve data/ files', (t) => {
+  // start test server
+  const app = express().use(express.static(testDataPath))
+  httpServer = app.listen()
+  const { address, port } = httpServer.address()
+  const baseUrl = `http://${address}:${port}`
+  testData = loadAllTestData(baseUrl)
   t.end()
 })
 
+test('isTargetHashValid', (t) => {
+  const confirmed = blockaiVerify(testData.confirmed)
+  const invalid = blockaiVerify(testData.invalid)
+  t.ok(confirmed.isTargetHashValid(), 'target hash is valid')
+  t.notOk(invalid.isTargetHashValid(), 'target hash is not valid')
+  t.end()
+})
+
+test('isMerkleRootValid', (t) => {
+  const confirmed = blockaiVerify(testData.confirmed)
+  const invalid = blockaiVerify(testData.invalid)
+  t.ok(confirmed.isMerkleRootValid(), 'merle root is valid')
+  t.notOk(invalid.isMerkleRootValid(), 'merkle root is not valid')
+  t.end()
+})
+
+test('close http server', (t) => {
+  httpServer.close()
+  t.end()
+})
