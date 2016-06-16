5806900c0d8e00c919a6ba8f11982c3b7cc059ac
Olivier Lalonde <olalonde@gmail.com>
2016-06-15 19:59:59 -0700

    fix logo in gh-pages

--- demo/src/app.js
+++ demo/src/app.js
@@ -8,7 +8,7 @@ const App = () => (
     <div className="row">
       <div className="text-center">
         <a href="https://blockai.com" style={{ margin: 20, display: 'block' }}>
-          <img width="200" src="/static/logo.png" alt="Blockai Verify" />
+          <img width="200" src="static/logo.png" alt="Blockai Verify" />
         </a>
         <h2>Blockai Proof Verification</h2>
         <br />
